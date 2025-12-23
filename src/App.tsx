a70ab7cd65fee1dec50dcbc45717e8b917408a6f
donaldo <donaldoprometeus@gmail.com>
2025-12-23 09:52:34 +0800

    fix: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SettingsPage from './pages/SettingsPage';
 import RenderPage from './pages/RenderPage';
 import Footer from './components/Footer';
 
 function App() {
-  const pathname = window.location.pathname;
-  if (pathname === '/render') {
-    return <RenderPage />;
-  }
-  
   return (
-    <>
-      <SettingsPage />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/render" element={<RenderPage />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <SettingsPage />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
